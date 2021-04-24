31d16e548d04f517e554ce5e3dbe8eb598d355fc
Vailderp <59770659+Vailderp@users.noreply.github.com>
2021-04-24 20:55:39 +0700

    Добавьте файлы проекта.

---
+++ Client/Client.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Client.h>
+
+int main(int argc, char* argv[])
+{
+	Client client(1111, "192.168.0.105");
+	
+	client.onAny(
+	[](const char* packet, SOCKET)
+	{
+		std::cout << packet << std::endl;
+	});
+
+	client.on("hello",
+		[&](const char* packet, SOCKET)
+		{
+			std::cout << "How are you?" << std::endl;
+			client.emit("I am good, and you?");
+		});
+	
+	client.start();
+
+	while (true)
+	{
+		
+	}
+
+	return EXIT_SUCCESS;
+}
